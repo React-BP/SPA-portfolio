d6f7da901c51ffa27b6ee29688dcf58d702513b8
Gena Riedler <genariedler@Genas-MacBook-Air.local>
2018-04-20 22:38:01 -0700

    committing wip

--- spas/src/App.js
+++ spas/src/App.js
@@ -1,19 +1,46 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Wrapper from "./components/Wrapper/Wrapper"
+import Navbar from "./components/Navbar/Navbar";
+import Billboard from "./components/Billboard/Billboard";
+import Items from './components/Items/Items';
+// import About from './components/About/About';
+import Card from "./components/Card/Card";
+import cardData from "./card-data.json";
+
+
 
 class App extends Component {
+  state = {
+    cardData: cardData
+  };
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Wrapper>
+        <Navbar /> 
+        <Billboard />
+        <Items />
+        {/* <About /> */}
+        <div className="card-box">
+            <div className="card-content">
+              Cards go here
+              <main className="card-grid">
+                {this.state.cardData.map(data => (
+                  <Card 
+                  key={data.id}
+                  img={data.img}
+                  alt={data.alt}
+                  title={data.title}
+                  desc={data.desc}
+                  button={data.button}
+                  />
+                ))}
+              </main>
+          </div>
+        </div>
+      </Wrapper>
+      </div>      
     );
   }
 }
---
+++ spas/src/components/Billboard/Billboard.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import './Billboard.css';
+import Arrow from '../Arrow/Arrow';
+
+
+import { Parallax, Background } from 'react-parallax';
+
+
+class Billboard extends Component {
+  render() {
+    return (
+    <div className="billboard">
+    <Parallax
+      blur={0}
+      bgImage={require('../../images/victoria-palacios-423143-unsplash.jpg')}
+      bgImageAlt="redwoods"
+      strength={200}
+    >
+      <div style={{ height: '400px' }} >
+        <div className="billboard-box">
+          <h1 className="display">React BP</h1>
+          <p className="lead">Boilerplate npm packages that contain components and functionality for a react portfolio app, a basic web store and a more complex web store. These packages contain everything you need to create a sophisticated react apps.</p>
+        <Arrow/>
+        </div>
+      </div>
+    </Parallax>
+    
+  </div>
+);}
+}
+export default Billboard;
